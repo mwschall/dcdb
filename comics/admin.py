500bc500a1f9ca7f67d782aacd5d7a1d2ac0cd2f
Matthew Schallberger <mwschall@gmail.com>
2019-03-30 23:38:46 -0700

    Render PDF pages when necessary.

--- comics/admin.py
+++ comics/admin.py
@@ -1,17 +1,12 @@
 import math
-import re
 from decimal import Decimal
-from io import BytesIO
 from os.path import splitext
 
-from PIL import Image
-from PyPDF2 import PdfFileReader
 from adminsortable2.admin import SortableInlineAdminMixin
 from django import forms
 from django.contrib import admin
 from django.contrib.admin import TabularInline
 from django.core.exceptions import ValidationError
-from django.core.files.uploadedfile import InMemoryUploadedFile
 from django.db import models
 from django.db import transaction
 from django.db.models import Window, F, Max
@@ -19,7 +14,8 @@ from django.db.models.functions import RowNumber
 from django.forms import Textarea
 
 from comics.forms import NumeralField, InstallmentFileField
-from comics.util import is_model_request, get_upload_fp
+from comics.importers import parse_pages, parse_pdf, get_pfr
+from comics.util import is_model_request
 from comics.validators import ARCHIVE_EXTS
 from .models import Installment, Series, Thread, ThreadSequence, Page, InstallmentLabel
 
@@ -72,11 +68,11 @@ class InstallmentAdminForm(forms.ModelForm):
 
             file_exts = {splitext(f.name)[1] for f in page_files}
 
-            if len(page_files) > 1 and file_exts & ARCHIVE_EXTS:
+            if len(page_files) > 1 and file_exts & set(ARCHIVE_EXTS):
                 raise ValidationError('Only one archive file at a time is accepted.')
 
             if '.pdf' in file_exts:
-                self.instance.page_count = PdfFileReader(get_upload_fp(page_files[0])).getNumPages()
+                self.instance.page_count = get_pfr(page_files[0]).getNumPages()
             elif '.zip' in file_exts:
                 raise ValidationError('Zip unpacking not implemented.')
             else:
@@ -105,43 +101,6 @@ class InstallmentLabelAdmin(admin.ModelAdmin):
 #########################################
 # Series Admin                          #
 #########################################
-
-def parse_pages(page_files):
-    has_cover = False
-    pages = []
-
-    for f in page_files:
-        if re.search('cover', f.name):
-            has_cover = True
-            pages.append({
-                'number': 0,
-                'file': f,
-            })
-        else:
-            m = re.search(r'^(?P<label>(?:[-_a-z\s]+(?:\d+[-_\s]+)?)?0*(?P<number>\d+)).*\.(?P<ext>[a-z1-9]+)$', f.name, re.I)
-            if m:
-                number = int(m.group('number'))
-                if number is 0:
-                    has_cover = True
-
-                label = m.group('label')[1:]
-                if label[0] == 'i' or label[0] == 'p':
-                    label = str(number)
-                else:
-                    label = label[0] + str(number)
-
-                pages.append({
-                    'label': label,
-                    'number': number,
-                    'file': f,
-                })
-
-    # assume single images are a strip or one-panel affair
-    if len(pages) <= 1:
-        has_cover = False
-
-    return pages, has_cover
-
 
 class InstallmentInline(SortableInlineAdminMixin, TabularInline):
     model = Installment
@@ -187,6 +146,8 @@ class SeriesAdmin(admin.ModelAdmin):
         if not form.is_valid():
             raise ValidationError('wut?')
         elif strip_files:
+            # TODO: this is now SUPER EXTRA busted
+
             pages, has_cover = parse_pages(strip_files)
             pages.sort(key=lambda pi: pi['file'].name)
 
@@ -215,65 +176,6 @@ class SeriesAdmin(admin.ModelAdmin):
 # Installment Admin                     #
 #########################################
 
-# https://stackoverflow.com/a/34116472
-def parse_pdf(pdf_file):
-    pages = []
-    pdf = PdfFileReader(get_upload_fp(pdf_file))
-
-    # TODO: shunt complex pages to poppler rather than erroring out
-    # TODO: make use of /ColorSpace resources in Pillow
-
-    for i, page in enumerate(pdf.pages):
-        x_object = page['/Resources']['/XObject'].getObject()
-
-        if '/Text' in page['/Resources']['/ProcSet']:
-            raise ValueError('Page %d specifies a /Text process.' % i)
-
-        obj = None
-        for o in x_object:
-            if x_object[o]['/Subtype'] == '/Image':
-                if obj:
-                    raise ValueError('Page %d has multiple images.' % i)
-                obj = o
-
-        if not obj:
-            raise ValueError('Page %d has no image.' % i)
-
-        fltr = x_object[obj]['/Filter']
-        size = (x_object[obj]['/Width'], x_object[obj]['/Height'])
-        data = x_object[obj].getData()
-        if x_object[obj]['/ColorSpace'] == '/DeviceRGB':
-            mode = "RGB"
-        else:
-            mode = "P"
-
-        buf = BytesIO()
-
-        if '/FlateDecode' in fltr:
-            x_object[obj] = Image.frombytes(mode, size, data)
-            x_object[obj].save(buf, 'PNG', optimize=True)
-            ext = ".pdf"
-            ct = 'image/png'
-        elif '/DCTDecode' in fltr:
-            buf.write(data)
-            ext = ".jpg"
-            ct = 'image/jpeg'
-        elif '/JPXDecode' in fltr:
-            buf.write(data)
-            ext = ".jpx"
-            ct = 'image/jpx'
-        else:
-            raise NotImplementedError('/Filter = %s not supported.' % fltr)
-
-        name = 'page_{:04d}{}'.format(i, ext)
-        pages.append({
-            'number': i,
-            'file': InMemoryUploadedFile(buf, None, name, ct, buf.tell(), None),
-        })
-
-    return pages, True
-
-
 @admin.register(Installment)
 class InstallmentAdmin(admin.ModelAdmin):
     form = InstallmentAdminForm
@@ -300,21 +202,22 @@ class InstallmentAdmin(admin.ModelAdmin):
         if page_files:
             ext = splitext(page_files[0].name)[1]
             if ext == '.pdf':
-                pages, has_cover = parse_pdf(page_files[0])
+                page_gen = parse_pdf(page_files[0])
             else:
-                pages, has_cover = parse_pages(page_files)
-            pages.sort(key=lambda pi: pi['number'])
+                page_gen = parse_pages(page_files)
 
-            obj.has_cover = has_cover
+            obj.has_cover = next(page_gen)
             obj.pages.all().delete()
             obj.save()
 
-            for idx, p in enumerate(pages):
+            idx = 0
+            for file in page_gen:
                 obj.pages.create(
                     order=idx,
-                    file=p['file'],
-                    original_name=p['file'].name,
+                    file=file,
+                    original_name=file.name,
                 )
+                idx += 1
         else:
             obj.save()
 
---
+++ comics/importers.py
@@ -0,0 +1,214 @@
+import io
+import math
+import mimetypes
+import re
+from os.path import getsize
+from pathlib import Path
+
+import PyPDF2
+import pyvips
+from PIL import Image as PilImage
+from django.core.files.uploadedfile import InMemoryUploadedFile, TemporaryUploadedFile
+
+from comics.util import get_upload_fp
+
+RE_NAME_SLICER = re.compile(r'^(?P<label>(?:[-_a-z\s]+(?:\d+[-_\s]+)?)?0*(?P<number>\d+)).*'
+                            r'\.(?P<ext>[a-z1-9]+)$',
+                            re.I)
+
+
+#########################################
+# Image Files                           #
+#########################################
+
+def parse_pages(page_files):
+    has_cover = False
+    pages = []
+
+    for f in page_files:
+        if re.search('cover', f.name):
+            has_cover = True
+            pages.append({
+                'number': 0,
+                'file': f,
+            })
+        else:
+            m = RE_NAME_SLICER.search(f.name)
+            if m:
+                number = int(m.group('number'))
+                if number is 0:
+                    has_cover = True
+
+                label = m.group('label')[1:]
+                if label[0] == 'i' or label[0] == 'p':
+                    label = str(number)
+                else:
+                    label = label[0] + str(number)
+
+                pages.append({
+                    'label': label,
+                    'number': number,
+                    'file': f,
+                })
+
+    # assume single images are a strip or one-panel affair
+    if len(pages) <= 1:
+        has_cover = False
+
+    pages.sort(key=lambda pi: pi['number'])
+
+    yield has_cover
+    yield from [p['file'] for p in pages]
+
+
+#########################################
+# PDF Files                             #
+#########################################
+
+def pt2px(pts, dpi=72):
+    return math.floor(pts * dpi / 72)
+
+
+def file_stem(pdf_file):
+    try:
+        return Path(pdf_file.name).stem
+    except AttributeError:
+        return 'page'
+
+
+def page_name(pdf_name, i, ext):
+    return '{}_{:04d}{}'.format(pdf_name, i, ext)
+
+
+def get_pfr(pdf_file):
+    # this is a non-trivial operation, and we can't close the tempfile anyway
+    if not hasattr(pdf_file, 'pfr'):
+        pdf_file.pfr = PyPDF2.PdfFileReader(get_upload_fp(pdf_file))
+    return pdf_file.pfr
+
+
+def get_page_info(pfr_page):
+    # figure out if the page is just a simple image
+    if '/Text' in pfr_page['/Resources']['/ProcSet']:
+        simple = False
+    else:
+        simple = True
+
+        x_object = pfr_page['/Resources']['/XObject'].getObject()
+
+        obj = None
+        for o in x_object:
+            if x_object[o]['/Subtype'] == '/Image':
+                if obj:
+                    simple = False
+                    break
+                obj = o
+
+        if not obj:
+            simple = False
+
+    crop_box = pfr_page.cropBox
+
+    return {
+        'simple': simple,
+        'width': crop_box[2] - crop_box[0],
+        'height': crop_box[3] - crop_box[1],
+    }
+
+
+# https://stackoverflow.com/a/54449010
+def convert_pdf(pdf_file, page_info, dpi=300, ext='.png', **kwargs):
+    # n is number of pages to load, -1 means load all pages
+    if hasattr(pdf_file, 'temporary_file_path'):
+        all_pages = pyvips.Image.new_from_file(pdf_file.temporary_file_path(),
+                                               dpi=dpi,
+                                               n=-1,
+                                               access="sequential")
+    else:
+        raise NotImplementedError('No ctype buffer/memory support.')
+
+    # That'll be RGBA ... flatten out the alpha
+    all_pages = all_pages.flatten(background=255)
+    print('Dims: {} x {}'.format(all_pages.width, all_pages.height))
+
+    # make sure we're not crazy
+    n_pages = all_pages.get("n-pages")
+    assert n_pages == len(page_info)
+
+    # the PDF is loaded as a very tall, thin image, with the pages joined
+    # top-to-bottom ... we loop down the image cutting out each page
+    pdf_name = file_stem(pdf_file)
+    content_type = mimetypes.guess_type('a'+ext)[0]
+    height_accum = 0
+    for i in range(0, n_pages):
+        print('Page {} at {}px'.format(i, height_accum))
+
+        # cut out our page
+        width = pt2px(page_info[i]['width'], dpi)
+        height = pt2px(page_info[i]['height'], dpi)
+        page = all_pages.crop(0, height_accum, width, height)
+        height_accum += height
+
+        # would have to deal with ctypes to do this in memory
+        name = page_name(pdf_name, i, ext)
+        with TemporaryUploadedFile(name, content_type, 0, None) as file:
+            page.write_to_file(file.temporary_file_path(), **kwargs)
+            file.size = getsize(file.temporary_file_path())
+
+            yield file
+
+
+# https://stackoverflow.com/a/34116472
+def rip_pdf(pdf_file, pfr):
+    pdf_name = file_stem(pdf_file)
+
+    # TODO: make use of /ColorSpace resources in Pillow if possible
+
+    for i, page in enumerate(pfr.pages):
+        x_object = page['/Resources']['/XObject'].getObject()
+
+        obj = None
+        for o in x_object:
+            if x_object[o]['/Subtype'] == '/Image':
+                obj = o
+                break
+        assert obj is not None
+
+        fltr = x_object[obj]['/Filter']
+        size = (x_object[obj]['/Width'], x_object[obj]['/Height'])
+        data = x_object[obj].getData()
+        if x_object[obj]['/ColorSpace'] == '/DeviceRGB':
+            mode = "RGB"
+        else:
+            mode = "P"
+
+        buf = io.BytesIO()
+
+        if '/FlateDecode' in fltr:
+            x_object[obj] = PilImage.frombytes(mode, size, data)
+            x_object[obj].save(buf, 'PNG', optimize=True)
+            ext = ".pdf"
+        elif '/DCTDecode' in fltr:
+            buf.write(data)
+            ext = ".jpg"
+        elif '/JPXDecode' in fltr:
+            buf.write(data)
+            ext = ".jpx"
+        else:
+            raise NotImplementedError('/Filter = %s not supported.' % fltr)
+
+        name = page_name(pdf_name, i, ext)
+        content_type = mimetypes.guess_type(name)[0]
+        yield InMemoryUploadedFile(buf, None, name, content_type, buf.tell(), None)
+
+
+def parse_pdf(pdf_file):
+    pfr = get_pfr(pdf_file)
+    page_info = [get_page_info(page) for page in pfr.pages]
+
+    yield True  # has_cover
+
+    if any([not pi['simple'] for pi in page_info]):
+        yield from convert_pdf(pdf_file, page_info, 150, ext='.jpg', Q=95)
+    else:
+        yield from rip_pdf(pdf_file, pfr)
